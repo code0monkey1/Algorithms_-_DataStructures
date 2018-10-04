baa41a1304a4dc7a6e4166357db5e18748ba8b98
Chiranjeev Thomas <chiranjeevcollege@gmail.com>
2018-10-04 17:33:49 +0530

    Added the Java implementation of 0/1 knapsack

---
+++ Algorithms/Dynaimic Programming/Java/Knapsack.java
@@ -0,0 +1,47 @@
+package algo;
+
+import java.util.Arrays;
+
+public class Knapsack {
+
+    public static int[] itemValue;
+    public static int[] itemWeight;
+    public static int knapsackWeight;
+    public static int items;
+    public static int dp[][];
+
+
+    public static void main(String[] args) {
+        knapsackWeight = 7;
+        items = 4;
+
+        itemValue = new int[items + 1];
+        itemWeight = new int[items + 1];
+
+        itemValue[1] = 1;
+        itemValue[2] = 4;
+        itemValue[3] = 5;
+        itemValue[4] = 7;
+
+        itemWeight[1] = 1;
+        itemWeight[2] = 3;
+        itemWeight[3] = 4;
+        itemWeight[4] = 5;
+
+        System.out.println(highestValueForKnapsack());
+    }
+
+    public static int highestValueForKnapsack() {
+
+        dp = new int[items + 1][knapsackWeight + 1];
+
+        for (int i = 1; i <= items; i++)
+            for (int k = 1; k <= knapsackWeight; k++)
+                dp[i][k] = Math.max(dp[i - 1][k],itemWeight[i]<=k? itemValue[i] + dp[i - 1][k-itemWeight[i]] : 0);
+
+
+        System.out.println(Arrays.deepToString(dp).replace("],", "]\n"));
+
+        return dp[items][knapsackWeight];
+    }
+}
